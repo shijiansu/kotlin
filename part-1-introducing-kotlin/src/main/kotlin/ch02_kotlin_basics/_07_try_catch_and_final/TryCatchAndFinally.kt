1c38d86a68e8a749baff97329eb1877be984dee1
Shijian Su <sushijian.iss.nus@gmail.com>
2019-07-15 23:55:00 +0800

    Add try catch

---
+++ part-1-introducing-kotlin/src/main/kotlin/ch02_kotlin_basics/_07_try_catch_and_final/TryCatchAndFinally.kt
@@ -0,0 +1,39 @@
+package ch02_kotlin_basics._07_try_catch_and_final
+
+import java.io.BufferedReader
+import java.io.StringReader
+
+fun readNumber(reader: BufferedReader): Int? {
+    try {
+        val line = reader.readLine()
+        return Integer.parseInt(line)
+    } catch (e: NumberFormatException) {
+        return null
+    } finally {
+        reader.close()
+    }
+}
+
+fun readNumber2(reader: BufferedReader) {
+    val number = try {
+        Integer.parseInt(reader.readLine())
+    } catch (e: NumberFormatException) {
+        return
+    }
+    println(number)
+}
+
+fun readNumber3(reader: BufferedReader) {
+    val number = try {
+        Integer.parseInt(reader.readLine())
+    } catch (e: NumberFormatException) {
+        null
+    }
+    println(number)
+}
+
+fun main(args: Array<String>) {
+    println(readNumber(BufferedReader(StringReader("239"))))
+    readNumber2(BufferedReader(StringReader("not a number")))
+    readNumber3(BufferedReader(StringReader("not a number")))
+}
